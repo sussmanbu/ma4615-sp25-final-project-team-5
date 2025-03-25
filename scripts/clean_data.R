257ef599ced03e4da014d13f1b8e3535f2ee256f
Thomas Laz <tvlaz@bu.edu>
2025-03-24 23:18:14 -0400

    Built site for gh-pages

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -2,7 +2,7 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(here))
 
-hiv_data <- read_csv(here::here("dataset", "HIV_Data_Original_With_Boroughs_Filled_In.csv"),
+hiv_data <- read_csv(here::here("dataset", "HIV_Data_Original_With_Modifications.csv"),
                      na = c("*", "NA"),
                      col_types = cols(
                        .default = col_double(),
@@ -14,11 +14,19 @@ hiv_data <- read_csv(here::here("dataset", "HIV_Data_Original_With_Boroughs_Fill
 
 hiv_data <- hiv_data |>
   mutate(
-    `RACE/ETHNICITY` = str_replace_all(`RACE/ETHNICITY`, "\n", " "), 
+    `RACE/ETHNICITY` = str_replace_all(`RACE/ETHNICITY`, "\n", " "),
+    `Borough` = str_replace_all(`Borough`, "\n", " "),
+    `Neighborhood (U.H.F)` = str_replace_all(`Neighborhood (U.H.F)`, "\n", " "),
     `RACE/ETHNICITY` = str_trim(`RACE/ETHNICITY`),
-    `RACE/ETHNICITY` = str_replace_all(`RACE/ETHNICITY`, "Latino/Hispanic", "Hispanic"))
+    `RACE/ETHNICITY` = str_replace_all(`RACE/ETHNICITY`, "Latino/Hispanic", "Hispanic"),
+    `Neighborhood (U.H.F)` = str_replace_all(`Neighborhood (U.H.F)`, "Union Square - Lower Eastside", "Union Square - Lower East Side"),
+    `Neighborhood (U.H.F)` = str_replace_all(`Neighborhood (U.H.F)`, "Upper East Side", "Upper Eastside"),
+    `Neighborhood (U.H.F)` = str_replace_all(`Neighborhood (U.H.F)`, "Upper West Side", "Upper Westside")
+    )
+
 
 hiv_clean <- hiv_data |>
   filter(complete.cases(hiv_data))
 
 write_rds(hiv_clean, here::here("dataset", "hiv_clean.rds"))
+
